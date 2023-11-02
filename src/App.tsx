bab87e81a6bd32b6136937ad5406a07d0dc4a910
Amal Joe R S <amaljoe88@gmail.com>
2023-11-02 11:43:52 +0530

    add employee form

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,12 @@ import "./css/table.css";
 import "./css/input.css";
 
 import { EmployeeListing } from "./views/EmployeeListing";
+import EmployeeForm from "./views/EmployeeForm";
 
 function App() {
   return (
     <div className="App">
-      <EmployeeListing />
+      <EmployeeForm />
     </div>
   );
 }
