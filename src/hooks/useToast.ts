840e2cdc31201ef80c3db48785ac47803dd1ab6a
Amal Joe R S <amaljoe88@gmail.com>
2023-11-21 09:28:46 +0530

    close toast on timeout

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -10,11 +10,14 @@ export default function useToast() {
       isError: false,
       id: Date.now(),
     };
+    setInterval(() => {
+      closeToast(toast.id);
+    }, 3000);
     setToasts([...toasts, toast]);
   };
 
   const closeToast = (id: number) => {
-    setToasts(toasts.filter((t) => t.id !== id));
+    setToasts((toasts) => toasts.filter((toast) => toast.id !== id));
   };
 
   return {
