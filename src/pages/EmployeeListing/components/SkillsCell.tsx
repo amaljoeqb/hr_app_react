690b7f8ccbbb311ab22cfd1bd791317f07645629
Amal Joe R S <amaljoe88@gmail.com>
2023-11-23 16:27:58 +0530

    update table styling

--- src/pages/EmployeeListing/components/SkillsCell.tsx
+++ src/pages/EmployeeListing/components/SkillsCell.tsx
@@ -15,7 +15,9 @@ export default function SkillsCell({ skills }: { skills: Skill[] }) {
   }
 
   function onMouseLeave() {
-    setIsActive(false);
+    if (isActive) {
+      setIsActive(false);
+    }
   }
 
   return (
