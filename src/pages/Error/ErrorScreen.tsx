f83b56ccbac8782a838e35bb78780ee17255153c
Amal Joe R S <amaljoe88@gmail.com>
2023-11-27 09:48:45 +0530

    Refactor ErrorScreen component to display a custom 404 page

--- src/pages/Error/ErrorScreen.tsx
+++ src/pages/Error/ErrorScreen.tsx
@@ -1,13 +1,15 @@
 import { useRouteError } from "react-router-dom";
+import { StyledErrorScreen } from "./ErrorScreen.style";
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    
-    return (
-        <div>
-            <h1>Oops!</h1>
-            <p>Something went wrong.</p>
-            <pre>{(error as Error)?.message}</pre>
-        </div>
-    );
+  const error = useRouteError();
+
+  return (
+    <StyledErrorScreen>
+      <h1>404</h1>
+      <h2>Page not found</h2>
+      <p>We are sorry but the page you are looking for does not exist.</p>
+      <pre>{(error as Error)?.message}</pre>
+    </StyledErrorScreen>
+  );
 }
