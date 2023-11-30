fea2d8af6576374fad28334822c33ed7f5a0984d
Amal Joe R S <amaljoe88@gmail.com>
2023-11-30 08:58:05 +0530

    Fix salary validation in employeeSchema.config.ts

--- src/config/employeeSchema.config.ts
+++ src/config/employeeSchema.config.ts
@@ -5,7 +5,6 @@ const employeeSchema = Yup.object({
   name: Yup.string().required("Required"),
   email: Yup.string().email("Invalid email address").required("Required"),
   salary: Yup.number()
-    .required("Required")
     .typeError("Salary must be a number")
     .min(0, "Salary can't be negative"),
   designation: Yup.string(),
