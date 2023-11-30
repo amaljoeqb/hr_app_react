cc3960393c3990bbbce9b948e58553ac40ca3a75
Amal Joe R S <amaljoe88@gmail.com>
2023-11-30 11:29:42 +0530

    set position of dropdown to fixed

--- src/components/ui/Dropdown/Dropdown.style.ts
+++ src/components/ui/Dropdown/Dropdown.style.ts
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 
-export const StyledDropdown = styled.div`
+export const StyledDropdown = styled.div<{ x: number; y: number }>`
   position: absolute;
   top: 100%;
   z-index: 100;
-  display: block;
 
   &.top {
     top: auto;
     bottom: 100%;
+  }
+
+  .dropdown {
+    position: fixed;
+    top: ${(props) => props.y}px;
+    left: ${(props) => props.x}px;
+    display: block;
   }
 `;
--- src/components/ui/Dropdown/Dropdown.tsx
+++ src/components/ui/Dropdown/Dropdown.tsx
@@ -6,26 +6,22 @@ export type DropdownPosition = "top" | "bottom";
 export function Dropdown(props: React.HTMLAttributes<HTMLDivElement>) {
   const ref = React.useRef<HTMLDivElement>(null);
   const [position, setPosition] = React.useState<DropdownPosition>("bottom");
+  const [coordinates, setCoordinates] = React.useState({ x: 0, y: 0 });
 
   React.useEffect(() => {
     const { current } = ref;
     if (current === null) return;
-    const isOverflowing =
-      current.scrollHeight > current.clientHeight ||
-      current.scrollWidth > current.clientWidth;
-
-    if (isOverflowing) {
-      console.log("The dropdown is overflowing.");
-    } else {
-      console.log("The dropdown is not overflowing.");
-    }
+    const { x, y } = current.getBoundingClientRect();
+    setCoordinates({ x, y });
   }, []);
 
-  function getClassName() {
-    let className = props.className || "";
-    className += ` ${position}`;
-    return className;
-  }
-
-  return <StyledDropdown ref={ref} {...props} className={getClassName()} />;
+  return (
+    <StyledDropdown
+      ref={ref}
+      {...coordinates}
+      className={`${position} dropdown`}
+    >
+      <div {...props} />
+    </StyledDropdown>
+  );
 }
